a3d5fee0d6984da29f5b9b9518c16e659fcf488e
Cleston Sousa <cleston.vs@gmail.com>
2021-05-23 12:23:38 -0300

    initial commit - spring boot, h2 based app

---
+++ src/main/java/br/net/gits/febraban/FebrabanGenApplication.java
@@ -0,0 +1,13 @@
+package br.net.gits.febraban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FebrabanGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FebrabanGenApplication.class, args);
+	}
+
+}
